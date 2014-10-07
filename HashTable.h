29ae7fa387d8be0be8473baf0e5d7ca9ae17a5ff
aryelle-player <playeral@miamioh.edu>
2014-10-07 12:53:14 -0400

    made more changes

--- HashTable.h
+++ HashTable.h
@@ -4,137 +4,151 @@
 #include "USet.h"
 
 /*
- * Note: Just above your template declaration when you use this class, you
- * must define method called "hash" that takes a Key as input, and returns
- * an unsigned long (which is the hash value)
- *
- * For example, you might do:
- * unsigned long hash(char c){ return 10*((unsigned long)c)%backingArraySize; }
- * HashTable<char,int> mySillyTable;
- *
- * If you don't define an appropriate hash function, the class won't compile.
- */
-
-/*
-template<class Key>
-unsigned long hash(Key k){
-	return n;
-};*/
+* Note: Just above your template declaration when you use this class, you
+* must define method called "hash" that takes a Key as input, and returns
+* an unsigned long (which is the hash value)
+*
+* For example, you might do:
+* unsigned long hash(char c){ return 10*((unsigned long)c)%backingArraySize; }
+* HashTable<char,int> mySillyTable;
+*
+* If you don't define an appropriate hash function, the class won't compile.
+*/
 
 
-	template <class Key, class T>
-	class HashTable : public USet < Key, T > {
-	private:
-		class HashRecord {
-		public:
-			Key k;
-			T x;
-
-			//If the slot in the hash table is totally empty, set this to true.
-			bool isNull;
-
-			//If the slot used to have something in it, but doesn't now, set
-			// isDel to true, and isNull to false. isNull is only for slots
-			// that have never been used
-			bool isDel;
-
-			HashRecord() { isNull = true; isDel = false; };
-		};
-
+template <class Key, class T>
+class HashTable : public USet < Key, T > {
+private:
+	class HashRecord {
 	public:
-		//See USet.h for documentation of these methods
-		virtual unsigned long size();
-		virtual void add(Key k, T x);
-		virtual void remove(Key k);
-		virtual T find(Key k);
-		virtual bool keyExists(Key k);
+		Key k;
+		T x;
 
-		//Initialize all private member variables.
-		HashTable();
-		//Delete any dynamically allocated memory.
-		virtual ~HashTable();
+		//If the slot in the hash table is totally empty, set this to true.
+		bool isNull;
 
-	private:
-		//A pointer to the array that holds the hash table data
-		HashRecord* backingArray;
+		//If the slot used to have something in it, but doesn't now, set
+		// isDel to true, and isNull to false. isNull is only for slots
+		// that have never been used
+		bool isDel;
 
-		//Whenever numItems + numRemoved >= backingArraySize/2, call
-		// grow(). grow() should make a new backing array that is twice the
-		// size of the old one, similar to what we did in the ArrayQueue
-		// lab.
-		//Note: You cannot just loop through the old array and copy it to the
-		// new one! Since the backing array size has changed, each item will likely
-		// map to a different slot in the array. You may just want to use add()
-		// after initializing the new array.
-		void grow();
-
-		//This helper method should take a key, and return the index for that
-		// item within the hash table. If the item already exists, return the
-		// index of the existing item. If the item doesn't exist, return the index
-		// where it OUGHT to be. This function can then be used as a helper method in
-		// your other methods.
-		unsigned long calcIndex(Key k);
-
-		unsigned long numItems; //Number of items in the hash table
-
-		//Note: Ordinarily, these OUGHT to be private. In this case I have
-		// made them public for easy of testing.
-	public:
-		unsigned long numRemoved; //Number of slots that have been removed but not re-used. Those that have isDel == true
-		unsigned long backingArraySize;
+		HashRecord() { isNull = true; isDel = false; };
 	};
 
-	//You will need this so you can make a string to throw in
-	// remove
+public:
+	//See USet.h for documentation of these methods
+	virtual unsigned long size();
+	virtual void add(Key k, T x);
+	virtual void remove(Key k);
+	virtual T find(Key k);
+	virtual bool keyExists(Key k);
+
+	//Initialize all private member variables.
+	HashTable();
+	//Delete any dynamically allocated memory.
+	virtual ~HashTable();
+
+private:
+	//A pointer to the array that holds the hash table data
+	HashRecord* backingArray;
+
+	//Whenever numItems + numRemoved >= backingArraySize/2, call
+	// grow(). grow() should make a new backing array that is twice the
+	// size of the old one, similar to what we did in the ArrayQueue
+	// lab.
+	//Note: You cannot just loop through the old array and copy it to the
+	// new one! Since the backing array size has changed, each item will likely
+	// map to a different slot in the array. You may just want to use add()
+	// after initializing the new array.
+	void grow();
+
+	//This helper method should take a key, and return the index for that
+	// item within the hash table. If the item already exists, return the
+	// index of the existing item. If the item doesn't exist, return the index
+	// where it OUGHT to be. This function can then be used as a helper method in
+	// your other methods.
+	unsigned long calcIndex(Key k);
+
+	unsigned long numItems; //Number of items in the hash table
+
+	//Note: Ordinarily, these OUGHT to be private. In this case I have
+	// made them public for easy of testing.
+public:
+	unsigned long numRemoved; //Number of slots that have been removed but not re-used. Those that have isDel == true
+	unsigned long backingArraySize;
+};
+
+//You will need this so you can make a string to throw in
+// remove
 #include <string>
 
-	template <class Key, class T>
-	HashTable<Key, T>::HashTable(){
-		//TODO
-	}
+template <class Key, class T>
+HashTable<Key, T>::HashTable(){
+  array<T> backingArray;
+  unsigned long numItems;
+  unsigned long numRemoved; 
+  unsigned long backingArraySize;
+}
 
-	template <class Key, class T>
-	HashTable<Key, T>::~HashTable() {
-		//TODO
-	}
+template <class Key, class T>
+HashTable<Key, T>::~HashTable() {
+	//TODO
+}
 
-	template <class Key, class T>
-	unsigned long HashTable<Key, T>::calcIndex(Key k){
-		//TODO
-		return numItems; //This indicates failure, since it is an impossible value
+template <class Key, class T>
+unsigned long HashTable<Key, T>::calcIndex(Key k){
+	for (int i = 0; i <backingArraySize; i++){
+		if(backingArray[i] == hash(k)){
+			return backingArray[i];
+		}else{
+			if(backingArray[i].isNull == true && backingArray[i].isDel == true){
+			return  hash(k)%backingArraySize;
+			}
+		}
 	}
+}
 
-	template <class Key, class T>
-	void HashTable<Key, T>::add(Key k, T x){
-		//TODO
+template <class Key, class T>
+void HashTable<Key, T>::add(Key k, T x){
+/*	for(int i = 0; i < backingArraySize; i++){
+		if(hash(k) == NULL){
+			
+		}
+	}*/
+}
+
+template <class Key, class T>
+void HashTable<Key, T>::remove(Key k){
+	//TODO
+}
+
+template <class Key, class T>
+T HashTable<Key, T>::find(Key k){
+	T dummy;
+	for (int i = 0; i < backingArraySize; i++){
+		if(backingArray[i] == hash(k)){
+			dummy = backingArray[i];
+		}
 	}
+	return dummy;
+}
 
-	template <class Key, class T>
-	void HashTable<Key, T>::remove(Key k){
-		//TODO
-	}
-
-	template <class Key, class T>
-	T HashTable<Key, T>::find(Key k){
-		//TODO
-		T dummy;
-		return dummy;
-	}
-
-	template <class Key, class T>
-	bool HashTable<Key, T>::keyExists(Key k){
-		//TODO
+template <class Key, class T>
+bool HashTable<Key, T>::keyExists(Key k){
+	if(find(k) == dummy){
 		return false;
-	}
+	}else{
+		return true};
+}	
 
-	template <class Key, class T>
-	unsigned long HashTable<Key, T>::size(){
-		//TODO
-		return 0;
-	}
+template <class Key, class T>
+unsigned long HashTable<Key, T>::size(){
+	//TODO
+	return 0;
+}
 
-	template <class Key, class T>
-	void HashTable<Key, T>::grow(){
-		//TODO
-	}
+template <class Key, class T>
+void HashTable<Key, T>::grow(){
+	//TODO
+}
 
